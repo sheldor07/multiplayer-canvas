83b661ed7893074d5d6598e886561f56e7119cf9
sheldor07 <50955708+sheldor07@users.noreply.github.com>
2023-01-14 16:36:33 +0800

    single player canvas(locally) completed
    Signed-off-by: sheldor07 <50955708+sheldor07@users.noreply.github.com>

---
+++ public/client.js
@@ -0,0 +1,14 @@
+let socket = io()
+let btnColorIt=document.getElementById('colorIt')
+socket.on('connect',()=>{
+    document.getElementById('socket-id').innerText=socket.id
+})
+
+function colorTheBox(color){
+    document.querySelector('.'+color).style.backgroundColor = color
+    setTimeout(()=>{document.querySelector('.'+color).style.backgroundColor = null},500)
+}
+btnColorIt.onclick = ()=>{
+    let color=document.getElementById('selectedColor').value
+    colorTheBox(color)
+}
